3fc0daeb37520bfeb8f3a7cc334bc3173b26756d
Vladimir Vakhter <vladimir.vakhter@gmail.com>
2021-08-13 13:58:23 -0400

    update the Square's render method

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ class Square extends React.Component {
     return (
       <button 
         className="square"
-        onClick={ () => { this.setState({value: 'X'}) }}
+        onClick={ () => { this.props.onClick() }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
